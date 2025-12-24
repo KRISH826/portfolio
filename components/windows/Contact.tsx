0e544f2502d7cf6900715af20d03a085e9b34f51
KRISH826 <krishnendupanja98@gmail.com>
2025-12-24 22:18:27 +0530

    feat: add Contact window component displaying social media links

--- components/windows/Contact.tsx
+++ components/windows/Contact.tsx
@@ -1,21 +1,33 @@
 "use client"
+import { socials } from '@/data/data';
 import WindowControls from '../WindowControls';
 import WindowWrapper from '../WindowWrapper';
+import Image from 'next/image';
 
 const Contact = () => {
     return (
         <>
-            <div className="rounded-xl overflow-hidden shadow-2xl bg-white">
-                {/* Window Header */}
-                <div id="window-header">
-                    <WindowControls windowKey="contact" />
-                    <h2>Contact</h2>
-                    <div className="w-14" />
-                </div>
-                {/* Window Body */}
-                <div className="bg-white p-5 font-mono text-sm min-h-[350px]">
-                    <p>Contact</p>
-                </div>
+            <div id="window-header">
+                <WindowControls windowKey="contact" />
+                <h2>Contact</h2>
+            </div>
+            {/* Window Body */}
+            <div className="bg-white p-5 space-y-4">
+                <h3>Let&apos;s Connect</h3>
+                <p>Got and idea? A bug to squash? Or just want to say hello?</p>
+
+                <ul className='mt-1'>
+                    {
+                        socials.map(({ id, bg, link, icon, text }) => (
+                            <li key={id} style={{ backgroundColor: bg }}>
+                                <a href={link} target="_blank" rel="noopener noreferrer">
+                                    <Image className='size-5' src={icon} alt={text} width={24} height={24} />
+                                    <p>{text}</p>
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </>
     )
